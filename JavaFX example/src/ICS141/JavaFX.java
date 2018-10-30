10d99a1f6faa2290f7b0db8b5529a013917970ad
aldiokenih <36461305+aldiokenih@users.noreply.github.com>
2018-10-29 19:47:42 -0500

    JavaFX
    Sample fo working with JavaFX

---
+++ JavaFX example/src/ICS141/JavaFX.java
@@ -0,0 +1,32 @@
+
+package ICS141;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application{
+
+	public void start(Stage primaryStage){
+		Button btOK = new Button("OK");
+
+		Scene scene = new Scene(btOK, 200, 250);
+		primaryStage.setTitle("myJavaFX");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public static void main (String[]args)
+	{
+		Application.launch(args);
+	}
+
+
+
+
+
+
+
+	}
+
---
+++ JavaFX example/src/ICS141/March28inclass.java
@@ -0,0 +1,60 @@
+package ICS141;
+
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.*;
+
+public class March28inclass extends Application{
+
+	public void start(Stage stage1){
+		Button btNam = new Button("Name");
+		Button btAddr = new Button("Address");
+
+		Scene scene1 = new Scene(btNam, 200, 250);
+		Scene scene2 = new Scene(btAddr, 200, 250);
+		stage1.setTitle("myJavaFX");
+		stage1.setScene(scene1);
+		stage1.show();
+
+		GridPane pane = new GridPane();
+		pane.setAlignment(Pos.CENTER);
+		pane.setPadding(new Insets( 11.5,12.5,13.5,14.5));
+		pane.setHgap(5.5);
+		pane.setVgap(5.5);
+
+		//place nodes in the pane
+		pane.add(new Label("First Name: "), 0, 0);
+		pane.add(new TextField(), 1, 0);
+		pane.add(new Label("Middle Intial: "), 0, 1);
+		pane.add(new TextField(), 1, 1);
+		pane.add(new Label("Last Name: "), 0, 2);
+		pane.add(new TextField(), 1, 2);
+		Button btAdd = new Button("Next");
+		pane.add(btAdd, 0, 3);
+		GridPane.setHalignment(btAdd, HPos.RIGHT);
+
+		//CREATE  ASCENE AND PLACE IT IN THE STAGE
+		Scene scene = new Scene(pane);
+		stage1.setTitle("ShowGridPane"); //set the stage title
+		stage1.setScene(scene);
+		stage1.show();
+
+
+
+	}
+	
+	public static void main (String[]args)
+	{
+		Application.launch(args);
+	}
+
+
+}
